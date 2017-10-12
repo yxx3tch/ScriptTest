bbdc00ee9dbba3d1221f352f4c11ce884ec31f8f
yxx3tch <yxx3tch@asagi.waseda.jp>
2017-10-12 09:02:51 +0900

    [add]class Boss

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss {        
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
+	}
+
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage+"のダメージを受けた" );
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+
+}
+
 public class Test : MonoBehaviour {
+	// 第一引数と第二引数の値を足した値を返す関数
+	int Add(int a, int b, int d){
+		// 第一引数と第二引数の値を足して、変数cに代入する
+		int c = a + b + d;
+		// 変数cを呼び出し元の関数に返す
+		return c;
+	}
 
 	// Use this for initialization
 	void Start()
 	{
-		// 配列を初期化する
-		int[] points = {30, 20, 50, 10, 80, 15, 60, 100, 1000};
-                
-		// 配列の要素数のぶんだけ処理を繰り返す
-		for (int i = 0; i < points.Length; i++) {
-			// 配列の要素が50以上の場合
-			if( points[i] >= 50 ){
-				// 配列の要素を表示する
-				Debug.Log (points [i]);
-			}
-		}
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+
+		// 攻撃用の関数を呼び出す
+		lastboss.Attack();
+		// 防御用の関数を呼び出す
+		lastboss.Defence(3);
+
+		Boss midboss = new Boss();
+		midboss.Attack();
+		midboss.Defence(30);
 	}
 
 	// Update is called once per frame
