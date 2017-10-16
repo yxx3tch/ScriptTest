419e261932c8c1fc9c47d020d891dcf9f67d4276
yxx3tch <yxx3tch@asagi.waseda.jp>
2017-10-17 07:58:40 +0900

    課題提出用
    配列の初期化、表示、BossクラスにMagic関数の実装、Start関数でMagic関数の呼び出し

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss {        
-	private int hp = 100;          // 体力
-	private int power = 25; // 攻撃力
+	private int hp = 100;       // 体力
+	private int power = 25; 	// 攻撃力
+	private int mp = 53; 		// 魔力
 
 	// 攻撃用の関数
 	public void Attack() { 
@@ -12,38 +13,61 @@ public class Boss {
 	}
 
 	// 防御用の関数
-	public  void Defence(int damage) { 
+	public void Defence(int damage) { 
 		Debug.Log( damage+"のダメージを受けた" );
 		// 残りhpを減らす
 		this.hp -= damage;
 	}
 
+	//　魔法用の関数
+	public void Magic() {
+		if (mp >= 5) {
+			// mpを5消費する
+			this.mp -= 5;
+			Debug.Log( "魔法攻撃をした。残りMPは" + mp + "。" );
+		} else {
+			Debug.Log( "MPが足りないため魔法が使えない。" );
+		}
+	}
 }
 
 public class Test : MonoBehaviour {
-	// 第一引数と第二引数の値を足した値を返す関数
-	int Add(int a, int b, int d){
-		// 第一引数と第二引数の値を足して、変数cに代入する
-		int c = a + b + d;
-		// 変数cを呼び出し元の関数に返す
-		return c;
+
+	void display(int[] input){
+		for (int i = 0; i < input.Length; i++){
+			Debug.Log(input[i]);
+		}
+	}
+
+	void display_invert(int[] input){
+		for (int i = input.Length - 1; i >= 0; i--){
+			Debug.Log(input[i]);
+		}
 	}
 
 	// Use this for initialization
 	void Start()
 	{
+		int[] array = {1, 2, 3, 4, 5};
+		display(array);
+		display_invert(array);
+
 		// Bossクラスの変数を宣言してインスタンスを代入
-		Boss lastboss = new Boss ();
+		Boss lastboss = new Boss();
+		Boss midboss = new Boss();
 
 		// 攻撃用の関数を呼び出す
 		lastboss.Attack();
+		midboss.Attack();
 		// 防御用の関数を呼び出す
 		lastboss.Defence(3);
-
-		Boss midboss = new Boss();
-		midboss.Attack();
 		midboss.Defence(30);
+
+		// 魔法用の関数を呼び出す
+		for (int i = 0; i < 11; i++){
+			lastboss.Magic();
 	}
+}
 
 	// Update is called once per frame
 	void Update () {
